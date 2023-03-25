24f606657d720e30e00bca6b77d00331f82ea83b
Sid624707 <sudharshan_thammaiahgari@epam.com>
2023-03-25 14:14:38 +0530

    Moved the business logic from controllers to sevices/index.js

--- routers/controllers/index.js
+++ routers/controllers/index.js
@@ -1,11 +1,9 @@
-const User=require('../../models/model.js')
-const { v4: uuidv4 } = require('uuid');
-const { Op } = require("sequelize");
+const services=require('../../services/index.js')
 
 async function findOne(req,res){
     const userId=req.params.id;
     try {
-        const user=await User.findOne({where:{id:userId}});
+        const user=await services.findUser(userId);
         if(user && !user.isDeleted){
             return res.status(200).json(user);
         }else{
@@ -14,19 +12,12 @@ async function findOne(req,res){
     } catch (error) {
         return res.status(400).json({"message":"Error in finding user"})
     }
-
 }
 
 async function createUser(req,res){
     const userData=req.body;
     try {
-        const newUser = await User.create({
-            id:uuidv4(),
-            login:userData.login,
-            password:userData.password,
-            age:userData.age,
-            isDeleted:userData.isDeleted
-        });
+        const newUser = await services.createUser(userData);
         return res.status(200).json(newUser);
     } catch (error) {
         return res.status(400).json(error.errors[0].message);
@@ -37,13 +28,8 @@ async function updateUser(req,res){
     const userData=req.body;
     const userId=req.params.id;
     try {
-        const user=await User.findByPk(userId);
+        const user=await services.updateUser(userId,userData);
         if(user && !user.isDeleted){
-            user.login=userData.login;
-            user.password=userData.password;
-            user.age=userData.age;
-            user.isDeleted=userData.isDeleted;
-            await user.save();
             return res.status(200).json(user);
         }
         else{
@@ -58,10 +44,8 @@ async function deleteUser(req,res){
     // Soft deleting the user
     const userId=req.params.id;
     try {
-        const user=await User.findByPk(userId);
-        if(user && !user.isDeleted){
-            user.isDeleted=true;
-            await user.save();
+        const user=await services.deleteUser(userId);
+        if(user && user.isDeleted){
             return res.status(200).json({"message":"User Deleted successfully."});
         }else{
             return res.status(400).json({"message":"User does not exist"});
@@ -75,15 +59,7 @@ async function getAutoSuggestUsers(req,res){
     const subString=req.params.subString;
     const limit = req.params.limit;
     try {
-        const users=await User.findAll({
-            where:{
-                login: {
-                    [Op.substring]:subString
-                },
-                isDeleted:false
-            },
-            limit:limit
-        });
+        const users=await services.getAutoSuggestUsers(subString,limit);
         if(users){
             return res.status(200).json(users);
         }else{
@@ -93,8 +69,6 @@ async function getAutoSuggestUsers(req,res){
         return res.status(400).json({"message":"Error in retreiving auto suggestions"})
     }
 }
-
-
 
 module.exports={
     findOne,
