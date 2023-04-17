853f553ce7c27ee731e855186433b591c7395fa2
Sid624707 <sudharshan_thammaiahgari@epam.com>
2023-04-17 11:37:20 +0530

    Initial commit

---
+++ src/manage/manage-user/manage-user.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/user';
+import { UserService } from 'src/user.service';
+
+@Component({
+  selector: 'app-manage-user',
+  templateUrl: './manage-user.component.html',
+  styleUrls: ['./manage-user.component.css']
+})
+export class ManageUserComponent {
+    users:User[]=[];
+    btnName="Details";
+    // selectedUser?:User;
+
+    constructor(public userService:UserService,private router:Router,private route:ActivatedRoute){
+
+    }
+
+    ngOnInit(){
+      this.getUsers();
+    }
+
+    getUsers():void{
+      this.users=this.userService.getUsers();
+    }
+
+    onSelect = (user:User):void=>{
+      // this.selectedUser=user;
+      // console.log(this.selectedUser);
+      this.router.navigate([user.id],{relativeTo:this.route})
+    }
+}
