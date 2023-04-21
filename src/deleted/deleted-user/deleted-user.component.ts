3ece44b617f893172bd723880711d2b7e921b19a
Sid624707 <sudharshan_thammaiahgari@epam.com>
2023-04-21 09:26:46 +0530

    Initial commit

---
+++ src/deleted/deleted-user/deleted-user.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { User } from 'src/user';
+import { UserService } from 'src/user.service';
+
+@Component({
+  selector: 'app-deleted-user',
+  templateUrl: './deleted-user.component.html',
+  styleUrls: ['./deleted-user.component.css']
+})
+export class DeletedUserComponent {
+   users:User[]=[];
+   btnName="Activate";
+   constructor(private userService: UserService){
+
+   }
+
+   ngOnInit() : void{
+      this.getDeletedUsers();
+   }
+
+   getDeletedUsers() : void{
+      // this.users= this.userService.getUsers().filter(user=>{
+      //   return user.isDeleted;
+      // })
+      this.userService.getUsers().subscribe(data=>{
+         this.users=data;
+      })
+   }
+
+   activate = (user:User):void =>{
+      // user.isDeleted=false;
+      // this.getDeletedUsers();
+      this.userService.activateUser(user.id).subscribe(user=>{
+         this.getDeletedUsers();
+      });
+   }
+}
